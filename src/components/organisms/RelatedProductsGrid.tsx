cb7de4882dbf52f484db3ac00717d4d2cc2a2019
Jan Lewandowski <jan.lewandowski@sailingbyte.com>
2023-10-15 17:09:53 +0200

    rm sleep

--- src/components/organisms/RelatedProductsGrid.tsx
+++ src/components/organisms/RelatedProductsGrid.tsx
@@ -2,11 +2,8 @@ import { ProductItem } from "../molecules/ProductItem";
 
 import { getProductsList } from "@/api/products";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export const RelatedProductsGrid = async () => {
 	const products = await getProductsList(1, 4);
-	await sleep(3000);
 
 	if (!products) {
 		return null;
--- src/components/organisms/SuggestedProductsGrid.tsx
+++ src/components/organisms/SuggestedProductsGrid.tsx
@@ -2,11 +2,8 @@ import { ProductsGrid } from "./ProductsGrid";
 
 import { getProductsList } from "@/api/products";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export const SuggestedProductsGrid = async () => {
 	const products = await getProductsList(1, 4);
-	await sleep(3000);
 
 	if (!products) {
 		return null;
